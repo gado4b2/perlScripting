f4de38cc466c3a078501641c3f5e933ae13ae14e
root <veerabhadrudu.s@gmail.com>
2018-12-05 19:07:27 +0530

    Perl Package Concepts Along With Separation Of Code Using Module Files

--- basics/perlPackageConceptsAlongWithSeparationOfCodeUsingModuleFiles.pl
+++ basics/perlPackageConceptsAlongWithSeparationOfCodeUsingModuleFiles.pl
@@ -3,16 +3,16 @@
 # This is a sample example which demonstrates using packages and seperation code into perl relocatable modules.
 # ! Observe the order of output lines of the program carefully to understand the concept of packages.
 # This example also explains about using keywords 'use' and 'require' to import packages using .pm(Perl Module) files.
-# Uncomment 'use Hello;' statement and comment 'require Hellow;' statement to see the difference between 'use' and 'require' keywords.
+# Uncomment 'use Hello;' statement and comment 'require Hello;' statement to see the difference between 'use' and 'require' keywords.
 # 'require' keyword will be executed during runtime phase of the program.
 # 'use' keyword is executed immediately after compiling the 'use' statement as described below.
 # 'use' PMFile; statement is equivalent to below block statement code.
-# BEGIN { require PMFile; if( isPackage_With_Name_PMFile_Exists_In_PMFile_And_a_subroutine_With_Name_default_In_PMFile_Pacakge ) { PMFileName::default();} }
+# BEGIN { require PMFile; if( isPackage_With_Name_PMFile_Exists_In_PMFile_And_a_subroutine_With_Name_default_In_PMFile_Package ) { PMFile::default();} }
 
 
 use strict;
 use warnings;
-#use Hello1;
+#use Hello;
 require Hello;
 
 sub Hello_Local () {
@@ -20,6 +20,8 @@ sub Hello_Local () {
 }
 
 Hello_Local();
+# Below subroutine 'hello()' is residing in Hello.pm file under no package.
+# If there are any subroutines in .pm files not under any pacakges inside .pm files, they will considered under main package by perl interpreter.
 hello();
 Hello::hello();
 HelloVariant::hello();
